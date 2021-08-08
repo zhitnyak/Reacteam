92c67a621a98df67e72f76577c35ef9d3296e128
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-08-08 12:08:10 +0300

    reload

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,39 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenulink1 = document.querySelector('.link1');
+  const closeMenulink2 = document.querySelector('.link2');
+  const closeMenulink3 = document.querySelector('.link3');
+  const closeMenulink4 = document.querySelector('.link4');
+  const closeMenulink5 = document.querySelector('.link5');
+  
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+    closeMenulink1.addEventListener('click', toggleMenu);
+    closeMenulink2.addEventListener('click', toggleMenu);
+    closeMenulink3.addEventListener('click', toggleMenu);
+  closeMenulink4.addEventListener('click', toggleMenu);
+      closeMenulink5.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
